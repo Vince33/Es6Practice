be48380fe28b9412499a43adbd5a7879c4aafe91
Vince Hines <vinjh3@gmail.com>
2016-07-25 15:12:46 -0400

    adds data table element

--- src/app.js
+++ src/app.js
@@ -4,9 +4,32 @@ import {Drone} from './classes/drone.js';
 import {fleet} from './fleet-data.js';
 import {FleetDataService} from './services/fleet-data-service.js';
 import {Button} from './ui/button.js';
+import {Image} from './ui/image.js';
+import {TitleBar} from './ui/title-bar.js';
+import {DataTable} from './ui/data-table.js';
 
-let b = new Button('Click Me');
-b.appendToElement($('body'));
+
+let headers = "License Make Model Miles".split(' ');
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+let dt = new DataTable(headers, dataService.cars);
+
+console.log(headers);
+dt.appendToElement($('body'));
+
+
+// let tb = new TitleBar('Acme Drones inc.');
+// tb.addLink('Home', '');
+// tb.addLink('Cars', '');
+// tb.addLink('Drones', '');
+// tb.addLink('Map', '');
+// tb.appendToElement($('body'));
+
+// let b = new Button('Click Me');
+// b.appendToElement($('body'));
+//
+// let i = new Image('../images/music-media-record-gramophone-phonograph-vinyl-rpm-audio.jpg');
+// i.appendToElement($('body'));
 
 
 
